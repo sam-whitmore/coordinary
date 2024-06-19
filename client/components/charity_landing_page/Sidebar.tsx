18b0a5273dd66e7420ebc99c22cc3dea5259c047
Sam Whitmore <sam@ritan.org.nz>
2024-06-20 10:28:32 +1200

    implement sign-in and sign-out buttons across all pages

--- client/components/charity_landing_page/Sidebar.tsx
+++ client/components/charity_landing_page/Sidebar.tsx
@@ -1,44 +1,54 @@
 import { Link } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import SignInButton from '../authentication/SignInButton'
+import SignOutButton from '../authentication/SignOutButton'
 
 export default function Sidebar() {
+  const { user } = useAuth0()
+
   return (
-    <div className="border-box flex h-[90%] w-full flex-col border-4 border-green-400">
-      <Link
-        to=""
-        className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
-      >
-        About Us
-      </Link>
-      <Link
-        to="posts"
-        className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
-      >
-        Our Work
-      </Link>
-      <Link
-        to="donate"
-        className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
-      >
-        Our Registries
-      </Link>
-      <Link
-        to="playground"
-        className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
-      >
-        Charity Playground
-      </Link>
-      <Link
-        to="sandbox"
-        className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
-      >
-        Charity Sandbox
-      </Link>
-      <Link
-        to="admin"
-        className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
-      >
-        Go to Admin Portal
-      </Link>
+    <div className="relative border-box flex h-[90%] w-full flex-col border-4 border-green-300">
+      <div className="border-box flex h-auto w-full flex-col border-4 border-purple-300">
+        <Link
+          to=""
+          className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
+        >
+          About Us
+        </Link>
+        <Link
+          to="posts"
+          className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
+        >
+          Our Work
+        </Link>
+        <Link
+          to="donate"
+          className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
+        >
+          Our Registries
+        </Link>
+        <Link
+          to="playground"
+          className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
+        >
+          Charity Playground
+        </Link>
+        <Link
+          to="sandbox"
+          className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
+        >
+          Charity Sandbox
+        </Link>
+        <Link
+          to="admin"
+          className="border-box mb-2 border-4 border-slate-200 bg-slate-50 p-2 shadow-md"
+        >
+          Go to Admin Portal
+        </Link>
+      </div>
+      <div className="absolute border-box border-4 border-sky-300 bottom-0 w-full text-center py-2">
+        { user ? <SignOutButton /> : <SignInButton />}
+      </div>
     </div>
   )
 }
