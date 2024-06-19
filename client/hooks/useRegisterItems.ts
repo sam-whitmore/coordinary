e54165542dd05267e38194d255e750d2859b9e53
Luke Pryor <djmiztic@gmail.com>
2024-06-20 11:21:22 +1200

    register & register items hooks created

---
+++ client/hooks/useRegisterItems.ts
@@ -0,0 +1,79 @@
+import request from 'superagent'
+import { useMutation } from '@tanstack/react-query'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { Register, RegisterData } from '../../models/register'
+
+const rootURL = '/api/v1/items'
+
+export default function useRegisterItems() {
+  function useGetAllRegisterItems() {
+    const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+
+    return useQuery({
+      queryKey: ['registerItems'],
+      queryFn: async () => {
+        const token = await getAccessTokenSilently()
+        if (!token) {
+          throw new Error(`Not logged in`)
+        }
+
+        const res = await request
+          .get(`${rootURL}`)
+          .auth(token, { type: 'bearer' })
+
+        return res.body as Register[]
+      },
+      enabled: isAuthenticated,
+    })
+  }
+
+  function useAddRegisterItems() {
+    const { getAccessTokenSilently } = useAuth0()
+    const queryClient = useQueryClient()
+
+    return useMutation({
+      mutationFn: async (register: RegisterData) => {
+        const token = await getAccessTokenSilently()
+        const authorizedResponse = {
+          ...register,
+          user_auth0_sub: token,
+        }
+        const res = await request
+          .post(`${rootURL}`)
+          .send(authorizedResponse)
+          .auth(token, { type: 'bearer' })
+
+        return res.body
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['registerItems'] })
+      },
+    })
+  }
+
+  function useDeleteRegisterItem() {
+    const { getAccessTokenSilently } = useAuth0()
+    const queryClient = useQueryClient()
+
+    return useMutation({
+      mutationFn: async (id: number) => {
+        const token = await getAccessTokenSilently()
+        const res = await request
+          .delete(`${rootURL}/${id}`)
+          .auth(token, { type: 'bearer' })
+
+        return res.body
+      },
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['registerItems'] })
+      },
+    })
+  }
+
+  return {
+    add: useAddRegisterItems().mutate,
+    allByRegister: useGetAllRegisterItems,
+    del: useDeleteRegisterItem().mutate,
+  }
+}
--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import * as Path from 'node:path'
-import fruitRoutes from './routes/fruits.ts'
 import charitiesRoutes from './routes/charities.ts'
 import itemsRoutes from './routes/items.ts'
 import registerRoutes from './routes/registers.ts'
@@ -11,7 +10,6 @@ const server = express()
 
 server.use(express.json())
 
-server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/charities', charitiesRoutes)
 server.use('/api/v1/items', itemsRoutes)
 server.use('/api/v1/registers', registerRoutes)
