5ed1bc381dd6c0e76ea77a3eb55603ad7ce3783e
Ali Harrison <tewairama212@gmail.com>
2024-06-20 10:43:02 +1200

    items route and functions matched to hopefully perfection

---
+++ server/db/items.ts
@@ -0,0 +1,57 @@
+import { ItemData, ItemSnakeCase } from '../../models/item'
+import db from './connection'
+
+// Pre-define snake_case to camelCase select statements for gets??
+const columns = [
+  'id',
+  'name',
+  'image',
+  'used',
+  'price_in_NZD as priceInNZD',
+  'NZD_raised as NZDRaised',
+]
+
+// Function to get all items
+export async function getAllItems() {
+  const result = await db('items').select(columns)
+  return result
+}
+
+// Function to get a specific item by ID
+export async function getItemById(id: number) {
+  const result = await db('items').where({ id }).select(columns).first()
+  return result
+}
+
+// Function to add a new item
+export async function addItem(data: Omit<ItemData, 'id'>) {
+  // Prevents typos when switching to snake_case
+  const snakeCase: ItemSnakeCase = {
+    name: data.name,
+    image: data.image,
+    used: data.used,
+    price_in_NZD: data.priceInNZD,
+    NZD_raised: data.NZDRaised,
+  }
+  const [id] = await db('items').insert(snakeCase)
+  return id
+}
+
+// Function to update an item by ID
+export async function updateItem(id: number, data: ItemData) {
+  const snakeCase: ItemSnakeCase = {
+    name: data.name,
+    image: data.image,
+    used: data.used,
+    price_in_NZD: data.priceInNZD,
+    NZD_raised: data.NZDRaised,
+  }
+  const result = await db('items').where({ id }).update(snakeCase)
+  return result
+}
+
+// Function to delete an item by ID
+export async function deleteItem(id: number) {
+  const result = await db('items').where({ id }).del()
+  return result
+}
