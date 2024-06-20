9fc31fcfd25d5576cb4414cb0094e3591131ab31
Luke Pryor <djmiztic@gmail.com>
2024-06-21 09:25:49 +1200

    register page data working

--- client/components/charity_admin_portal/AdminHome.tsx
+++ client/components/charity_admin_portal/AdminHome.tsx
@@ -1,6 +1,6 @@
 export default function AdminHome() {
   return (
-    <div className="w-5/6 h-full border-box border-4 border-green-400">
+    <div className="border-box h-full w-5/6 border-4 border-green-400">
       <h1>Admin Home</h1>
     </div>
   )
--- client/components/charity_landing_page/registries/Registry.tsx
+++ client/components/charity_landing_page/registries/Registry.tsx
@@ -1,8 +1,27 @@
+import useRegisters from '../../../hooks/useRegisters.ts'
+
 export default function Registry() {
+  const { data, isPending, isError } = useRegisters().allByRegister()
+
+  if (isPending) {
+    ;<div>Loading...</div>
+  }
+
+  if (isError) {
+    console.error(isError)
+  }
+
   return (
-    <div className="w-full h-[90%] border-box border-4 border-pink-400">
+    <div className="border-box h-[90%] w-full border-4 border-pink-400">
       <h1>Registry</h1>
-      <p>This component contains the currently selected gift; donation register.</p>
+      <p>
+        This component contains the currently selected gift; donation register.
+        {data?.map((register) => (
+          <li key={register.id} className="list-none">
+            {register.name}
+          </li>
+        ))}
+      </p>
     </div>
   )
 }
