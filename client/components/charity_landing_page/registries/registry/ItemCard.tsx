690dbb3bb351b3d29ba9bea507c233fb8f203155
Sam Whitmore <sam@ritan.org.nz>
2024-06-21 14:25:15 +1200

    update ItemFromRegister to include name, as well as the DB function; add name to ItemCard.tsx

--- client/components/charity_landing_page/registries/registry/ItemCard.tsx
+++ client/components/charity_landing_page/registries/registry/ItemCard.tsx
@@ -3,7 +3,7 @@ import { ItemFromRegister } from "../../../../../models/item"
 export default function ItemCard(item: ItemFromRegister) {
   return (
     <div className="w-full h-36 border-box border-4 border-sky-400">
-      <h1>{item.priceInNZD}</h1>
+      <h1>{item.name}</h1>
     </div>
   )
 }
