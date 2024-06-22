dcc651abc627b6d2526c40ec43c3481b7f7bf3ca
Luke Pryor <djmiztic@gmail.com>
2024-06-22 19:53:07 +1200

    itemcard css stuff

--- client/components/charity_landing_page/registries/registry/ItemCard.tsx
+++ client/components/charity_landing_page/registries/registry/ItemCard.tsx
@@ -6,8 +6,8 @@ export default function ItemCard(item: ItemFromRegister) {
   return (
     <div className="rounded-2xl border border-black text-center shadow-xl">
       <img
-        className="mx-auto"
-        src="https://i.imgur.com/RpKHLSx.gif"
+        className="mx-auto mb-4 mt-4 rounded-full border border-black"
+        src="https://placehold.co/125x125"
         alt={item.name}
       ></img>
       <h1 className="text-2xl">
@@ -15,9 +15,9 @@ export default function ItemCard(item: ItemFromRegister) {
         <span className="text-sm">{item.used === 1 ? '(Used)' : ''}</span>
       </h1>
       <div className="text-center">{progressBarWidth} Funded!</div>
-      <div className="mx-auto mt-2 h-4 w-3/4 rounded-2xl bg-gray-200 shadow-xl">
+      <div className="mx-auto mt-2 h-6 w-3/4 rounded-2xl bg-gray-300 shadow-xl">
         <div
-          className="h-4 rounded-2xl bg-gradient-to-r from-blue-500 to-green-500 shadow-xl"
+          className="h-6 rounded-2xl bg-gradient-to-r from-purple-500 to-amber-500 shadow-xl"
           style={{ width: progressBarWidth }}
         ></div>
         <div className="mt-2 flex justify-between text-sm text-gray-600">
@@ -30,11 +30,15 @@ export default function ItemCard(item: ItemFromRegister) {
           perferendis nam placeat consequuntur sit impedit quaerat, vero maiores
           id accusamus.
         </div>
-        <div className="mt-4">
+        <div className="mt-6 flex justify-between">
+          <button className="rounded border border-black px-4 py-2 text-blue-500 hover:bg-blue-700">
+            More Info
+          </button>
           <button className="rounded border border-transparent bg-blue-500 px-4 py-2 text-white hover:bg-blue-700">
             Donate
           </button>
         </div>
       </div>
+    </div>
   )
 }
