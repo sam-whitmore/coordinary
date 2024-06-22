2301a0daefb38d14c85e2894c1eed7fe449217bc
Sam Whitmore <sam@ritan.org.nz>
2024-06-22 16:27:08 +1200

    add styling to homepage and About page

--- client/components/home_page/CharitiesList.tsx
+++ client/components/home_page/CharitiesList.tsx
@@ -7,14 +7,14 @@ export default function CharitiesList() {
   if (isPending) console.log('awaiting data...')
   if (isError) return console.error(`Error: ${error.message}`)
   if (!charities) return (
-    <div className="w-1/4 h-full overflow-y-scroll border-l-2 border-primary">
-      <h2 className="underline underline-offset-2 text-right px-8 mt-4 font-display text-2xl">Charities</h2>
+    <div className="w-1/4 h-full overflow-y-scroll border-b-2 border-l-2 border-secondary">
+      <h2 className="underline underline-offset-2 text-center px-8 mt-4 font-display text-2xl">Charities</h2>
       <p>Error: no charities found.</p>
     </div>
   )
   return (
-    <div className="w-1/4 h-full overflow-y-scroll border-l-2 border-primary">
-      <h2 className="underline underline-offset-2 text-right px-8 mt-4 font-display text-2xl">Charities</h2>
+    <div className="w-1/4 h-full overflow-y-scroll border-b-2 border-l-2 border-secondary">
+      <h2 className="text-center px-8 mt-4 font-display text-2xl text-primary">Charities in Tauranga</h2>
       {charities.map((charity) => {return <CharitySummary key={charity.id} {...charity} />})}
     </div>
   )
--- client/components/home_page/Nav.tsx
+++ client/components/home_page/Nav.tsx
@@ -7,34 +7,34 @@ export default function Nav() {
   const { user } = useAuth0()
 
   return (
-    <div className="flex h-[10%] min-h-[60px] max-h-[80px] w-full justify-between border-b-2 border-primary">
+    <div className="flex h-[10%] min-h-[60px] max-h-[80px] w-full justify-between border-b-2 border-secondary">
       <div className="mx-2 my-auto p-2">
-        <button className="font-display text-3xl font-medium text-primary underline-offset-2 hover:underline">
+        <Link to="/" className="font-display text-3xl font-medium text-secondary hover:text-primary">
           Coordinary
-        </button>
+        </Link>
       </div>
       <div className="flex w-auto">
         <Link
           to="coordinary"
-          className="my-auto px-4 font-display text-lg text-secondary underline-offset-2 hover:underline"
+          className="my-auto px-4 font-display text-lg text-secondary hover:text-primary"
         >
           About Us
         </Link>
         <Link
           to="coordinary/posts"
-          className="my-auto px-4 font-display text-lg text-secondary underline-offset-2 hover:underline"
+          className="my-auto px-4 font-display text-lg text-secondary hover:text-primary"
         >
           Our Posts
         </Link>
         <Link
           to="coordinary/donate"
-          className="my-auto px-4 font-display text-lg text-secondary underline-offset-2 hover:underline"
+          className="my-auto px-4 font-display text-lg text-secondary hover:text-primary"
         >
           Our Register
         </Link>
         <Link
           to="coordinary/contact"
-          className="my-auto px-4 font-display text-lg text-secondary underline-offset-2 hover:underline"
+          className="my-auto px-4 font-display text-lg text-secondary hover:text-primary"
         >
           Contact Us
         </Link>
