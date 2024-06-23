2d0dc3424fff54c9a2ecb74a68a0776a46af6389
Hamiora Keelan <Hamiora.Keelan@gmail.com>
2024-06-23 16:28:43 +1200

    fix user profile car positioning

--- client/components/nav/ProfileCard.tsx
+++ client/components/nav/ProfileCard.tsx
@@ -5,14 +5,17 @@ import { User } from '@auth0/auth0-react'
 export default function ProfileCard(user: User) {
   return (
     <div className="flex h-full w-full rounded-xl">
-      <Link to="donor/admin" className="text-xl font-display text-secondary my-auto mx-2 pl-2 hover:text-primary">
+      <Link
+        to="donor/admin"
+        className="mx-2 my-auto pl-2 font-display text-xl text-secondary hover:text-primary"
+      >
         My Portal
       </Link>
       <SignOutButton />
       <img
         src={user.picture}
         alt={user.name}
-        className="h-full w-auto rounded-full p-2"
+        className="h-20 w-20 rounded-full p-2"
       />
     </div>
   )
