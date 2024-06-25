479f6099f3c754a26e2b713337f3536e1ed81bf7
Hamiora Keelan <158783153+hamiora-keelan@users.noreply.github.com>
2024-06-25 14:26:38 +1200

    center charity cards and add follow button

--- client/components/home_page/charities_list/CharitySummary.tsx
+++ client/components/home_page/charities_list/CharitySummary.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { Charity } from '../../../../models/charity'
 import useCharities from '../../../hooks/useCharitiesInformation'
-
+import FollowCharityButton from '../../../utility/FollowCharityButton'
 export default function CharitySummary(charity: Charity) {
   const {
     data: charityInfo,
@@ -22,14 +22,19 @@ export default function CharitySummary(charity: Charity) {
     return <p>Error: Charity not found.</p>
   }
 
+  const donorId = 1 // Replace with actual donorId
+
   return (
-    <div className="w-100 h-168 mx-2 my-4 flex flex-col justify-between overflow-hidden rounded-2xl border-2 border-secondary bg-white shadow-lg hover:border-primary">
-      <div>
+    <div className="relative mx-auto my-4 flex w-full max-w-xs flex-col justify-between overflow-hidden rounded-2xl border-2 border-secondary bg-white shadow-lg hover:border-primary">
+      <div className="relative">
         <img
           src={charityInfo.image || 'uploads/placeholder.png'}
           alt={charity.name}
           className="h-64 w-full rounded-t-2xl object-cover"
         />
+        <div className="absolute right-2 top-2">
+          <FollowCharityButton donorId={donorId} charityId={charity.id} />
+        </div>
         <div className="p-4">
           <Link
             to={`/${charity.slug}`}
@@ -38,9 +43,11 @@ export default function CharitySummary(charity: Charity) {
             {charity.name}
           </Link>
           <h3 className="mt-4 text-lg font-semibold text-secondary">Vision</h3>
-          <p className="mt-2 text-sm italic ">{charityInfo.vision}</p>
+          <p className="mt-2 text-sm italic text-secondary">
+            {charityInfo.vision}
+          </p>
           <h3 className="mt-4 text-lg font-semibold text-secondary">Mission</h3>
-          <p className="mt-2 text-sm ">{charityInfo.mission}</p>
+          <p className="mt-2 text-sm text-secondary">{charityInfo.mission}</p>
         </div>
       </div>
     </div>
